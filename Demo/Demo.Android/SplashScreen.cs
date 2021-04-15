2c62faa6ce4b7aa12a52adc2657720c0d3c71c24
Johnson Joseph Nadar <56380952+johnson555100@users.noreply.github.com>
2021-04-15 18:10:25 +0530

    Add project files.

---
+++ Demo/Demo.Android/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo.Droid
+{
+
+    [Activity( Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true )]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            System.Threading.Thread.Sleep(80);
+            StartActivity(typeof(MainActivity));
+
+        }
+    }
+}
---
+++ Demo/Demo/Views/FirstPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Demo.ViewModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace Demo.Views
+{
+    /// <summary>
+    /// Page to show the no item
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FirstPage" /> class.
+        /// </summary>
+        public FirstPage()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDMwOTUwQDMxMzkyZTMxMmUzMElMMzI0Q0lGdERCOWVzOFlEcWd2aThVSUZWak1lZmpJSXVWeTRCSGt3Ykk9");
+            this.InitializeComponent();
+            this.BindingContext = FirstPageViewModel.BindingContext;
+        }
+
+        private void OpenScanner(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        public async void Scanner()
+        {
+
+            var ScannerPage = new ZXingScannerPage();
+
+            ScannerPage.OnScanResult += (result) => {
+
+                ScannerPage.IsScanning = false;
+
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    DisplayAlert("Scanned", result.Text, "OK");
+                });
+            };
+
+            await Navigation.PushAsync(ScannerPage);
+
+        }
+    }
+}
